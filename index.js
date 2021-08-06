bd3ff9299f8fcc1d60a7b9e96f7e7ef2b90f66f0
Osama Chegem <adiga_knight@hotmail.com>
2021-08-06 21:31:13 +0300

    added port

--- index.js
+++ index.js
@@ -41,4 +41,4 @@ app.post( '/delete', ( req, res ) => {
 } )
 
 
-app.listen( 3000, () => console.log( "server started" ) )
+app.listen( process.env.PORT || 3000, () => console.log( "server started" ) )
